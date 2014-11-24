43f41f8bba1871d52cd30762e4515a5adfed7071
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-11-23 22:17:22 -0500

    Add fix for double looping over .out files

--- build_data.sh
+++ build_data.sh
@@ -9,23 +9,24 @@ do
 	for path in opt/{b3lyp,cam,m06hf} noopt;
 	do
 		echo $molset $path
-		geoms_out=$main/$path/geoms/out
+		geoms=$main/$path/geoms
+		geoms_out=$geoms/out
 		mkdir -p $geoms_out
-		geoms_mol2=$main/$path/geoms/mol2
+		geoms_mol2=$geoms/mol2
 		mkdir -p $geoms_mol2
 
 		# Build INDO input files (simple xyz format)
-		cp -f $molset/$path/b3lyp/*.gjf $geoms_out
-		for f in `ls $geoms_out`;
+		cp -f $molset/$path/b3lyp/*.gjf $geoms
+		for f in `ls $geoms`;
 		do
-			mv "$geoms_out/${f}" "$geoms_out/${f/_TD/}" 2>/dev/null;
+			mv "$geoms/${f}" "$geoms/${f/_TD/}" 2>/dev/null;
 		done
-		for f in `ls $geoms_out/ | sed -e 's/.gjf//'`;
+		for f in `ls $geoms/ | sed -e 's/.gjf//'`;
 		do
-			mv -f "$geoms_out/$f.gjf" "$geoms_out/$f.out" 2>/dev/null;
+			mv -f "$geoms/$f.gjf" "$geoms/$f.out" 2>/dev/null;
 		done
-		sed -i -e '1,/^0 1$/d' -e '/^$/d' -e '/^1 /,$d' $geoms_out/*;
-
+		sed -i -e '1,/^0 1$/d' -e '/^$/d' -e '/^1 /,$d' $geoms/*.out;
+		mv $geoms/*.out $geoms_out/
 
 		# Build mol2 files for use in fingerprinting
 		for f in `ls $geoms_out`;

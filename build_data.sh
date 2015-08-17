fad5cb964fad169169250a37c333bba8a9cb6cda
Christopher Collins <crcollins@cmu.edu>
2015-08-16 23:02:49 -0400

    Make the build script a little easier to read

--- build_data.sh
+++ build_data.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 rm -rf mol_data/
-main=mol_data
+main="mol_data"
+repulse_data="projects/repulse/unix/results"
 
 if [ -z "$1" ] ;
 then
-	base=mols
+	base="mols"
 else
-	base=$1
+	base="$1"
 fi
-mkdir -p $main
+mkdir -p "$main"
 
 for molset in $(ls $base);
 do
@@ -24,25 +25,28 @@ do
 		datadir=$base/$molset
 
 		# Build INDO input files (simple xyz format)
+		# This only has to be done per opt set
 		cp -f $datadir/$path/b3lyp/*.gjf $geoms
-		for f in `ls $geoms`;
+		for f in $(ls $geoms | sed -e 's/.gjf//');
 		do
-			mv "$geoms/${f}" "$geoms/${f/_TD/}" 2>/dev/null;
+			# Rename all files to get rid of _TD
+			mv "$geoms/$f.gjf" "$geoms/${f/_TD/}.out" 2>/dev/null;
 		done
-		for f in `ls $geoms/ | sed -e 's/.gjf//'`;
-		do
-			mv -f "$geoms/$f.gjf" "$geoms/$f.out" 2>/dev/null;
-		done
+
+		# Remove the everything except the geometry from the gjfs
 		sed -i -e '1,/^0 1$/d' -e '/^$/d' -e '/^1 /,$d' $geoms/*.out;
 		mv $geoms/*.out $geoms_out/
 
 		# Build mol2 files for use in fingerprinting
-		for f in `ls $geoms_out`;
+		for f in $(ls $geoms_out);
 		do
-			x=$(wc -l $geoms_out/$f | sed -e 's/ .*$//'); sed -e "1s/^/$x\n\n/" $geoms_out/$f > $geoms_mol2/$f;
+                        # Create real xyz file for conversion to mol2
+			# The xyz format starts with the number of atoms
+			x=$(wc -l $geoms_out/$f | sed -e 's/ .*$//');
+                        sed -e "1s/^/$x\n\n/" $geoms_out/$f > $geoms_mol2/$f;
 		done
 
-		for f in `ls $geoms_mol2 | sed -e '/.mol2/d'`;
+		for f in $(ls $geoms_mol2 | sed -e '/.mol2/d');
 		do
 			babel -ixyz $geoms_mol2/$f -omol2 $geoms_mol2/${f/.out/}.mol2 > /dev/null 2>&1;
 		done
@@ -67,15 +71,17 @@ with open("$main/$path/$molset/$subpath.csv", "r") as f, open("$main/$path/$mols
 EOF
 		done
 
+		# Fix to adjust the poor sorting done by `sort`
 		SCRIPT="from sys import stdin, stdout; stdout.writelines(sorted(stdin, key=lambda x: x.split()[0]))"
-		for f in `ls projects/repulse/unix/results/$path/$molset/indo*.txt | sed -e's/.*indo/indo/'` ;
+		for f in $(ls $repulse_data/$path/$molset/indo*.txt | sed -e's/.*indo/indo/') ;
 		do
 			python -c "$SCRIPT" < projects/repulse/unix/results/$path/$molset/$f > $main/$path/$molset/$f ;
 		done
 	done
 done
 
-find $main -name "*.txt" | xargs -P$(nproc) sed -i -e '/Using multi/d' -e '/100.0 100.0 100.0/d' -e '/Repulse/d'
+# Delete bad lines from repulse data
+find $main -name "*.txt" | xargs -P$(nproc) sed -i -e '/Using multi/d;/100.0 100.0 100.0/d;/Repulse/d'
 
 tar -cjf mol_data.tar.bz2 mol_data
 
